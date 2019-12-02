0de2fdb0ee0b1636fe628a25ea2e118a54f7730c
Patton Parham <patton.parham@verizon.net>
2019-12-02 01:53:25 -0600

    working on some errors in calling the api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 var APIkey = "3fc6abd45549271dc494fafbdad4de6e";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + APIkey;
+
+$.ajax({
+url: queryURL,
+method: "GET"
+}).then(function(response){
+
+console.log(queryURL);
+console.log(response);
+
+
+
+
+
+});
