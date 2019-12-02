8f0efe80db710f17f5fa0f016c4c8de94b0c5305
Patton Parham <patton.parham@verizon.net>
2019-12-02 03:30:45 -0600

    definitely able to access the information using the current weather and 5day forecast apis. GET method was used. Working on how to get the icon to display. Still have not completed any search function. Have been focusing on just getting the information to display for one city.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 var APIkey = "3fc6abd45549271dc494fafbdad4de6e";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + APIkey;
-
+let WeatherID = ""
+var queryURLtoday = "https://api.openweathermap.org/data/2.5/weather?id=4644585&appid=3fc6abd45549271dc494fafbdad4de6e" ;
+var queryURLfiveday = "https://api.openweathermap.org/data/2.5/forecast?id=4644585&appid=3fc6abd45549271dc494fafbdad4de6e"
 $.ajax({
-url: queryURL,
+url: queryURLtoday,
 method: "GET"
 }).then(function(response){
+console.log(APIkey)
+console.log(queryURLtoday);
 
-console.log(queryURL);
 console.log(response);
 
-
-
-
+$(".cityName").text(response.name)
+$(".temp").text(response.main.temp)
+$(".humidity").text(response.main.humidity)
+$(".weatherIcon").text(response.weather[0])//maybe .image? didn't work
+$(".windSpeed").text(response.wind.speed)
 
 });
+
+$.ajax({
+url: queryURLfiveday,
+method: "GET"
+
+}).then(function(response){
+console.log(queryURLfiveday);
+console.log(response);
+
+})
