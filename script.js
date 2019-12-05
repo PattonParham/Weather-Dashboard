95f5947c1e4dab155d3b946a390c37bf7efbcd9d
Patton Parham <patton.parham@verizon.net>
2019-12-05 17:41:16 -0600

    added in Kelvin to Farenheight conversion for temps

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ console.log(queryURLtoday);
 console.log(response);
 
 $(".cityName").text(response.name)
-$(".temp").text(response.main.temp)
-$(".humidity").text(response.main.humidity)
+$(".temp").text("Temp: " + ((response.main.temp - 273.15) * 1.80 + 32))
+$(".humidity").text("Humidity: " + response.main.humidity)
 $(".weatherIcon").text(response.weather[0])//maybe .image? didn't work
-$(".windSpeed").text(response.wind.speed)
+$(".windSpeed").text("Windspeed: " + response.wind.speed)
 
 });
 
@@ -36,25 +36,25 @@ method: "GET"
 console.log(queryURLfiveday);
 console.log(response);
 //temps for five day
-$(".dayonetemp").text(response.list[0].main.temp)
-$(".daytwotemp").text(response.list[8].main.temp)
-$(".daythreetemp").text(response.list[16].main.temp)
-$(".dayfourtemp").text(response.list[24].main.temp)
-$(".dayfivetemp").text(response.list[32].main.temp)
+$(".dayonetemp").text("Temp: " + ((response.list[0].main.temp - 273) *1.80 +32))
+$(".daytwotemp").text("Temp: " + ((response.list[8].main.temp - 273) *1.80 +32))
+$(".daythreetemp").text("Temp: " + ((response.list[16].main.temp - 273) *1.80 +32))
+$(".dayfourtemp").text("Temp: " + ((response.list[24].main.temp - 273) *1.80 +32))
+$(".dayfivetemp").text("Temp: " + ((response.list[0].main.temp - 273) *1.80 +32))
 
 //humidity for five day
-$(".dayonehumidity").text(response.list[0].main.humidity)
-$(".daytwohumidity").text(response.list[8].main.humidity)
-$(".daythreehumidity").text(response.list[16].main.humidity)
-$(".dayfourhumidity").text(response.list[24].main.humidity)
-$(".dayfivehumidity").text(response.list[32].main.humidity)
+$(".dayonehumidity").text("Humidity: " + response.list[0].main.humidity)
+$(".daytwohumidity").text("Humidity: " + response.list[8].main.humidity)
+$(".daythreehumidity").text("Humidity: " + response.list[16].main.humidity)
+$(".dayfourhumidity").text("Humidity: " + response.list[24].main.humidity)
+$(".dayfivehumidity").text("Humidity: " + response.list[32].main.humidity)
 
 //windspeed for five day
-$(".dayonewindspeed").text(response.list[0].wind.speed)
-$(".daytwowindspeed").text(response.list[8].wind.speed)
-$(".daythreewindspeed").text(response.list[16].wind.speed)
-$(".dayfourwindspeed").text(response.list[24].wind.speed)
-$(".dayfivewindspeed").text(response.list[32].wind.speed)
+$(".dayonewindspeed").text("Windspeed: " + response.list[0].wind.speed)
+$(".daytwowindspeed").text("Windspeed: " + response.list[8].wind.speed)
+$(".daythreewindspeed").text("Windspeed: " + response.list[16].wind.speed)
+$(".dayfourwindspeed").text("Windspeed: " + response.list[24].wind.speed)
+$(".dayfivewindspeed").text("Windspeed: " + response.list[32].wind.speed)
 
 $(".fiveday").text(response.city.name) ;
 
