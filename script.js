d775224cd10e309fd97d3bc79a642d906a80696a
Patton Parham <patton.parham@verizon.net>
2019-12-05 17:16:33 -0600

    some formatting with bootstrap acheived, some issue with var inputText = .val();not grabbing the text from the input of the cityname ID

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var APIkey = "3fc6abd45549271dc494fafbdad4de6e";
 let WeatherID = ""
-var queryURLtoday = "https://api.openweathermap.org/data/2.5/weather?id=4644585&appid=3fc6abd45549271dc494fafbdad4de6e" ;
-var queryURLfiveday = "https://api.openweathermap.org/data/2.5/forecast?id=4644585&appid=3fc6abd45549271dc494fafbdad4de6e"
+
+
+var queryURLtoday = "https://api.openweathermap.org/data/2.5/weather?q=" + inputText + ",US&appid=3fc6abd45549271dc494fafbdad4de6e";
+var queryURLfiveday = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputText + ",US&appid=3fc6abd45549271dc494fafbdad4de6e";
+var inputText = $("#cityName").val();
+console.log(inputText)
+
+$("#submitBtn").on("click", function(event){
+
+event.preventDefault();
+
 $.ajax({
 url: queryURLtoday,
 method: "GET"
@@ -26,5 +35,31 @@ method: "GET"
 }).then(function(response){
 console.log(queryURLfiveday);
 console.log(response);
+//temps for five day
+$(".dayonetemp").text(response.list[0].main.temp)
+$(".daytwotemp").text(response.list[8].main.temp)
+$(".daythreetemp").text(response.list[16].main.temp)
+$(".dayfourtemp").text(response.list[24].main.temp)
+$(".dayfivetemp").text(response.list[32].main.temp)
 
+//humidity for five day
+$(".dayonehumidity").text(response.list[0].main.humidity)
+$(".daytwohumidity").text(response.list[8].main.humidity)
+$(".daythreehumidity").text(response.list[16].main.humidity)
+$(".dayfourhumidity").text(response.list[24].main.humidity)
+$(".dayfivehumidity").text(response.list[32].main.humidity)
+
+//windspeed for five day
+$(".dayonewindspeed").text(response.list[0].wind.speed)
+$(".daytwowindspeed").text(response.list[8].wind.speed)
+$(".daythreewindspeed").text(response.list[16].wind.speed)
+$(".dayfourwindspeed").text(response.list[24].wind.speed)
+$(".dayfivewindspeed").text(response.list[32].wind.speed)
+
+$(".fiveday").text(response.city.name) ;
+
+
+
+
+})
 })
